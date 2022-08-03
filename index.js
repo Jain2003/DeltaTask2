2277862a1e7b2df6eabf431aed2346f71bacf279
Jain2003 <95215600+Jain2003@users.noreply.github.com>
2022-08-03 09:46:24 +0530

    complete

--- index.js
+++ index.js
@@ -1,186 +1,206 @@
-var canvas = document.querySelector("canvas");
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-var c = canvas.getContext("2d");
-const platformPositionX = [175,1225,675,25,935];
-const platformPositionY = [325,425,575,675,725];
-var gravity = 0.6;
-var friction = 0.7;
-var batch = 1;
-var platformPositionInMotionX = [175,1225,675,25,975];
-var platformPositionInMotionY = [225,325,475,575,675];
-var order = [3,0,4,2,1]
-var user = false;
-var userX = [];
-var userY = [];
-// making the top Nails
-var keys = {
-        right: false,
-        left: false,
-        up: false,
-        };
-function Player(xPosition,yPosition,widthUser,heightUser){
-  this.yPosition = yPosition;
-  this.xPosition = xPosition;
-  this.widthUser = widthUser;
-  this.heightUser = heightUser;
-  this.dxUser = 0;
-  this.dyUser = 0;
-  this.jump = true;
-  this.drawUser = function(){
-    c.fillStyle = "red";
-    c.fillRect(this.xPosition , this.yPosition , this.widthUser , this.heightUser, this.dxUser , this.dyUser);
-  }
-  this.updateUser = function(){
-    this.drawUser();
-          // Updating the y and x coordinates of the player
-          player.yPosition -= player.dyUser;
-          player.xPosition += player.dxUser;
-          if(player.xPosition < window.innerWidth - player.widthUser ){
-            if(keys.right) {
-                player.dxUser = 1.5;
-            }
-          }
-          if(player.xPosition  > player.widthUser ){
-            // If the left key is pressed increase the relevant horizontal velocity
-            if(keys.left) {
-                player.dxUser = -1.5;
-            }
-          }
-          if(player.yPosition >window.innerHeight - player.heightUser ){
-            // If the player is not jumping apply the effect of frictiom
-            if(player.jump == false) {
-                player.dxUser *= friction;
-            } else {
-                // If the player is in the air then apply the effect of gravity
-                player.dyUser += gravity;
-            }
-            player.jump = true;
-          }
-    // if(user === false){
-    //   this.yPosition -= this.dyUser;
-    //   // console.log(this.yPosition);
-    //   if(this.yPosition < heightTop){
-    //     location.reload();
-    //   }
-    // }
-  }
-}
-var player = new Player(225,455,20,20,1,1);
-document.addEventListener("keydown",function(e){
-  console.log("keydown");
-  if(e.keyCode == 37) {
-    console.log("left movement");
-      keys.left = true;
-      console.log(e);
-      // player.updateUser();
-  }
-  if(e.keyCode == 38) {
-      if(player.jump == false) {
-          player.dyUser = -1;
-      }
-      // player.updateUser();
-  }
-  if(e.keyCode == 39) {
-      keys.right = true;
-  }
-  player.updateUser();
-})
+let score = 0;
+let lives = 0;
+const gravity = 3;
+const platformMoveVel = 7;
+const keys = {
+  right: {
+    pressed: false,
+  },
+  left: {
+    pressed: false,
+  },
+  up: {
+    pressed: false,
+  },
+};
 
-document.addEventListener("keyup",function(e){
-console.log("keyup");
-  if(e.keyCode == 37) {
-      keys.left = false;
-      // player.updateUser();
-  }
-  if(e.keyCode == 38) {
-      // if(player.dyUser < -2) {
-      player.dyUser = -1;
-      // }
-      // player.updateUser();
-  }
-  if(e.keyCode == 39) {
-      keys.right = false;
-  }
-  player.updateUser();
-})
+var scoreCount = document.querySelector(".scoreCount").innerHTML;
+var livesCount = document.querySelector(".livesCount").innerHTML;
 
+// construting and drawing top spikes
 var xTop = 0;
 var yTop = 0;
 var heightTop = 50;
 var widthTop = 100;
-for(var i = 0 ; i<=window.innerWidth/widthTop ; i++){
-    c.beginPath();
-    c.moveTo(xTop, yTop);
-    c.lineTo(xTop + widthTop, yTop);
-    c.lineTo(xTop + widthTop/2, yTop + heightTop);
-    c.fillStyle = "#019267";
-    c.fill();
-    xTop+=widthTop;
-}
-// // making the bottom Nails
-var xBottom = 0;
-var yBottom = window.innerHeight;
-var heightBottom = 50;
-var widthBottom = 100;
-for(var j = 0 ; j<=window.innerWidth/widthBottom ; j++){
+for (var i = 0; i <= window.innerWidth / widthTop; i++) {
   c.beginPath();
-  c.moveTo(xBottom , yBottom);
-  c.lineTo(xBottom+widthBottom , yBottom);
-  c.lineTo(xBottom + widthBottom/2 , yBottom - heightBottom);
-  c.fillStyle = "#019267";
+  c.moveTo(xTop, yTop);
+  c.lineTo(xTop + widthTop, yTop);
+  c.lineTo(xTop + widthTop / 2, yTop + heightTop);
+  c.fillStyle = "#2C3333";
   c.fill();
-  xBottom+=widthBottom;
-}
-var rectangleArray=[];
-for(var i = 0 ; i<platformPositionInMotionX.length ; i++){
-  rectangleArray.push(new Rectangle(platformPositionInMotionX[i] , platformPositionInMotionY[i] , 1 , 200 , 15));
+  xTop += widthTop;
 }
 
-
-//  now i will create a js object in which I will make multiple platforms
-function Rectangle(x,y,dy,rectWidth,rectHeight){
-  this.x = x;
-  this.y = y;
-  this.dy = dy;
-  this.rectWidth = rectWidth;
-  this.rectHeight = rectHeight;
-  this.draw = function(){
-    c.fillStyle = "#82954B";
-    c.fillRect(this.x , window.innerHeight-2*this.rectHeight - this.y , this.rectWidth , this.rectHeight);
+function Ball() {
+  this.location = {
+    x: canvas.width / 2 + 25,
+    y: canvas.height / 2 - 25,
+  };
+  this.vel = {
+    dx: 0,
+    dy: 0,
   }
-  this.update = function(){
-    this.draw();
-    if(this.y > window.innerHeight - 2*heightTop){
-      var i = 0;
-      this.rectHeight = 0;
-      this.rectWidth = 0;
+  this.drawingBall = function() {
+    c.fillStyle = "blue";
+    c.beginPath();
+    c.arc(this.location.x, this.location.y, 25, 0, Math.PI * 2);
+    c.strokeStyle = "purple";
+    c.stroke();
+    c.fill();
+    c.closePath();
+  }
+  this.updatingBall = function() {
+    this.location.y += this.vel.dy;
+    this.vel.dy += gravity;
+    if (this.location.x +25 >= canvas.width) {
+      this.vel.dx = 0;
+    } else if (this.location.x - 25 <= 0) {
+      this.vel.dx = 0;
     }
-    else{
-      this.y += this.dy;
-    }
+    this.drawingBall();
+  }
+}
+
+function Platform(x_location, y_location) {
+  // console.log("is this happening?");
+  this.location = {
+    x: x_location,
+    y: y_location
+  };
+  this.height = 15;
+  this.width = 180;
+  if (screen.width <= 360) {
+    this.width = 90;
   }
 
+  this.drawingPlatform = function() {
+    c.fillstyle = "#EF5B0C";
+    c.beginPath();
+    c.fillRect(this.location.x, this.location.y, this.width, this.height);
+    c.closePath();
+  }
 }
 
-// generating new platformPosition
-var platformUpdatedY = [180 , 110 , 70 , 150 ,200];
-function generateNewPlatforms(){
-  setInterval(function(){
-    for(var i = 0 ; i<platformPositionInMotionX.length ; i++){
-      rectangleArray.push(new Rectangle(Math.random()*window.innerWidth , platformUpdatedY[i] , 1, 200 , 15));
-    }
-  },4000*batch)
-  batch++;
+function HealthPack(x_location, y_location) {
+  this.location = {
+    x: x_location,
+    y: y_location,
+  }
+  this.drawingPack = function() {
+    c.fillstyle = "pink";
+    c.beginPath();
+    c.arc(this.location.x, this.location.y, 20, 0, Math.PI * 2);
+    c.fill();
+    c.closePath();
+  }
 }
-generateNewPlatforms();
-// animate function
-function animate(){
+
+const ball = new Ball();
+const platforms = [];
+const healthPacks = [];
+
+let yCoordinate = 200;
+let xCoordinate = 0;
+let left = true;
+
+// constructing the coordinates for platforms and the health packs on the platforms
+for (var i = 0; i < 10000; i++) {
+  if (left) {
+    xCoordinate = Math.floor((Math.random() * canvas.width)/2);
+    left = false;
+  } else {
+    xCoordinate = Math.floor(Math.random() * canvas.width / 2 + canvas.width / 2 - 50);
+    left = true;
+  }
+  platforms.push(new Platform(xCoordinate, yCoordinate));
+
+  if (i % 25 === 0) {
+    healthPacks.push(new HealthPack(xCoordinate + Math.floor(Math.random()*180), yCoordinate - 25 ));
+  }
+  yCoordinate += 150;
+}
+
+function animate() {
   requestAnimationFrame(animate);
-  c.clearRect(0 , yTop+heightTop , window.innerWidth , window.innerHeight - 2*heightBottom);
-  for(var i = 0 ; i<rectangleArray.length ; i++){
-    rectangleArray[i].update();
+  c.clearRect(0, yTop, canvas.width, canvas.height );
+  ball.updatingBall();
+  // the following code are conditions for the movement of the ball.
+  if (keys.right.pressed) {
+    ball.vel.dx += 3;
+    ball.location.x += ball.vel.dx;
+  } else if (keys.left.pressed) {
+    ball.vel.dx += 3;
+    ball.location.x -= ball.vel.dx;
+  } else if (keys.up.pressed) {
+    ball.vel.dy = -3;
   }
-  player.updateUser();
+  if (
+    ball.location.y - 25 <= yTop + heightTop ||
+    ball.location.y + 25 >= screen.height
+  ) {
+    console.log("You Lost");
+    scoreCount--;
+  }
+  console.log(platforms);
+  platforms.forEach(function(platform) {
+    console.log(platforms);
+    platform.drawingPlatform();
+    if (
+      ball.location.y  <= (platform.location.y-25) &&
+      ball.location.y + ball.vel.dy >= (platform.location.y - 25) &&
+      ball.location.x >= platform.location.x &&
+      ball.location.x   <= platform.location.x + platform.width
+    ) {
+      ball.vel.dy = 0;
+    }
+    platform.location.y -= platformMoveVel;
+  });
+  ball.location.y -= platformMoveVel;
+  //
+  for (var i = 0; i < healthPacks.length; i++) {
+    healthPacks[i].drawingPack();
+    healthPacks[i].location.y -= platformMoveVel;
+
+    if (Math.ceil(healthPacks[i].location.y)+25 === Math.ceil(ball.location.y)) {
+
+      if ((ball.location.x - healthPack[i].location.x) <= 50 || ball.location.x - health[i].location.x >=-50) {
+        healthPacks.splice(i, 1);
+      }
+    }
+  }
 }
+
 animate();
+
+// the below piece of code is to control the ball.
+window.addEventListener("keydown", function(e) {
+  if (e.code === "ArrowLeft") {
+    keys.left.pressed = true;
+  } else if (e.code === "ArrowRight") {
+    keys.right.pressed = true;
+  } else if (e.code === "ArrowUp") {
+    keys.up.pressed = true;
+  }
+});
+
+window.addEventListener("keyup", function(e) {
+  if (e.code === "ArrowLeft") {
+    keys.left.pressed = false;
+    ball.vel.dx = 0;
+  } else if (e.code === "ArrowRight") {
+    keys.right.pressed = false;
+    ball.vel.dx = 0;
+  } else if (e.code === "ArrowUp") {
+    keys.up.pressed = false;
+    ball.vel.dy = 0;
+  }
+});
+
+const scoremanaging = setInterval(function(){
+  scoreCount++;
+},1000)
